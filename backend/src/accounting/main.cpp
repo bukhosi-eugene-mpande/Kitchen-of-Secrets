7b2a058f60def5651405d78717ac54ef8c1e5143
Ashley Kapaso <ashleykapaso@gmail.com>
2023-10-23 14:52:27 +0200

    test strategy billing

--- backend/src/accounting/main.cpp
+++ backend/src/accounting/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+#include "Billing/Billing.h"
+#include "Billing/CashBill.h"
+// #include "Billing/CardBill.h"
+
 int main()
 {
     std::cout << "Accounting Says Hi" << std::endl;
+
+    Billing *billing = new Billing(new CashBill(100));
+    billing->pay(50);
 
     return 0;
 }
