8de1db37e118d70d88e6be91303c2f1c59b82573
Ashley Kapaso <ashleykapaso@gmail.com>
2023-10-29 10:25:12 +0200

    install mui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { purple, orange } from '@mui/material/colors';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
 import Start from './pages/Start/Start';
 import Staff from './pages/Staff/Staff';
 import Customer from './pages/Customer/Customer';
 import NotFound from './pages/NotFound/NotFound';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: purple[600]
+      },
+      secondary: {
+        main: orange[600]
+      }
+    }
+  });
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Start />} />
-        <Route path='*' element={<NotFound />} />
-        <Route index path='/staff' element={<Staff />} />
-        <Route index path='/customer' element={<Customer />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path='/' element={<Start />} />
+          <Route path='*' element={<NotFound />} />
+          <Route index path='/staff' element={<Staff />} />
+          <Route index path='/customer' element={<Customer />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
