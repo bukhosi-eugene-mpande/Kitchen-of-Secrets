a623fbf81f590f671d76f38548da58edab7fc551
Ashley Kapaso <ashleykapaso@gmail.com>
2023-10-29 10:02:47 +0200

    setup react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Start from './pages/Start/Start';
+import Staff from './pages/Staff/Staff';
+import Customer from './pages/Customer/Customer';
+import NotFound from './pages/NotFound/NotFound';
+
 function App() {
-  return <h1>Kitchen Of Secrets</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='*' element={<NotFound />} />
+        <Route index path='/staff' element={<Staff />} />
+        <Route index path='/customer' element={<Customer />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/pages/Customer/Customer.jsx
+++ frontend/src/pages/Customer/Customer.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function Customer() {
   return <h1>Customer</h1>;
 }
