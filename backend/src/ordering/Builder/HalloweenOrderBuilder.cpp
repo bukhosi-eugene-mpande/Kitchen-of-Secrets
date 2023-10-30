0c0f47de8df6418ac605edbc7b2978f894f436b5
ayandajuqu <ms.ayandajuqu@gmail.com>
2023-10-30 16:43:55 +0200

    added the functionalities of the HalloweenOrderBuilders

--- backend/src/ordering/Builder/HalloweenOrderBuilder.cpp
+++ backend/src/ordering/Builder/HalloweenOrderBuilder.cpp
@@ -4,5 +4,13 @@
 void HalloweenOrderBuilder::addDrink()
 {
     Menu* drinksMenu=drinksMenuFactory.createMenu();
-    
+    this->order->items.insert(drinksMenu->getItems().begin(), drinksMenu->getItems().end());
+    delete drinksMenu;
+}
+
+void HalloweenOrderBuilder::addFood()
+{
+    Menu* foodMenu=foodMenuFactory.createMenu();
+    this->order->items.insert(foodMenu->getItems().begin(), foodMenu->getItems().end());
+    delete foodMenu;
 }
