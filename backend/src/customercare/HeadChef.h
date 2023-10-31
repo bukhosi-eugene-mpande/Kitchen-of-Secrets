5185d59736d787e351841396addf02462dbb8437
Chenoa <chenoa.perkett@gmail.com>
2023-10-31 10:18:02 +0200

    observer updated

---
+++ backend/src/customercare/HeadChef.h
@@ -0,0 +1,22 @@
+#ifndef HeadChef_H
+#define HeadChef_H
+
+#include <vector>
+#include <string>
+
+
+class HeadChef{
+    private:
+    std::string name;
+
+    public:
+
+        HeadChef();
+
+        ~HeadChef();
+
+        void cancelOrder();
+
+};
+
+#endif
