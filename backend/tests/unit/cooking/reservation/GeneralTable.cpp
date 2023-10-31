b934b79a877acc429c62d00f45ece064eff5468f
Gini24mp <u21573558@tuks.co.za>
2023-10-31 11:05:43 +0200

    removal of vpp files to prevent merge conflicts

--- backend/tests/unit/cooking/reservation/GeneralTable.cpp
+++
@@ -1,6 +0,0 @@
-#include "GeneralTable.h"
-#include "../customercare/Customer.h"
-
-GeneralTable::GeneralTable() : Table("General Section",4) { }
-
-GeneralTable::~GeneralTable() { }
--- backend/tests/unit/cooking/reservation/PrivateTable.cpp
+++
@@ -1,7 +0,0 @@
-#include "PrivateTable.h"
-#include "../customercare/Customer.h"
-
-PrivateTable::PrivateTable() : Table("Private Section",6) { }
-
-PrivateTable::~PrivateTable() { }
-
