64e1db51cfd09759f9faf93efe650200311d1a99
ayandajuqu <ms.ayandajuqu@gmail.com>
2023-11-02 18:09:43 +0200

    tested the FoodTests

--- backend/tests/FoodTests.cpp
+++ backend/tests/FoodTests.cpp
@@ -13,21 +13,21 @@ TEST(FoodTest, ConstructorTest) {
     
 }
 
-// TEST(FoodTest, CloneTest) {
-//     std::unordered_map<std::string, int> ingredients = {{"flour", 2}, {"sugar", 1}, {"eggs", 3}};
-//     Food originalFood(10.99, "Pancakes", ingredients);
+TEST(FoodTest, CloneTest) {
+    std::unordered_map<std::string, int> ingredients = {{"flour", 2}, {"sugar", 1}, {"eggs", 3}};
+    Food originalFood(10.99, "Pancakes", ingredients);
     
-//     // Clone the originalFood
-//     std::shared_ptr<MenuItem> clonedFood = originalFood.clone();
+    // Clone the originalFood
+    std::shared_ptr<MenuItem> clonedFood = originalFood.clone();
 
-//     // Check if the clone is an instance of the Food class
-//     std::shared_ptr<Food> foodClone = std::dynamic_pointer_cast<Food>(clonedFood);
-//     ASSERT_NE(foodClone, nullptr);
+    // Check if the clone is an instance of the Food class
+    std::shared_ptr<Food> foodClone = std::dynamic_pointer_cast<Food>(clonedFood);
+    ASSERT_NE(foodClone, nullptr);
 
-//     // Check the price and name of the clone
-//     EXPECT_EQ(foodClone->calculatePrice(), originalFood.calculatePrice());
-//     EXPECT_EQ(foodClone->getName(), originalFood.getName());
+    // Check the price and name of the clone
+    EXPECT_EQ(foodClone->calculatePrice(), originalFood.calculatePrice());
+    EXPECT_EQ(foodClone->getName(), originalFood.getName());
 
-//     // Check the ingredients of the clone
-//     EXPECT_EQ(foodClone->calculateIngredients(), originalFood.calculateIngredients());
-// }
+    // Check the ingredients of the clone
+    EXPECT_EQ(foodClone->calculateIngredients(), originalFood.calculateIngredients());
+}
