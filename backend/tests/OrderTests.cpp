b921b79b2de32de5b2efce326c32dd8e305135d4
ayandajuqu <ms.ayandajuqu@gmail.com>
2023-11-03 08:40:37 +0200

    added & tested Order,CalculateIngredientsTest

--- backend/tests/OrderTests.cpp
+++ backend/tests/OrderTests.cpp
@@ -40,3 +40,21 @@ TEST(OrderTest, CalculatePriceTest) {
     // Check if the calculated price matches the expected sum of meal prices
     EXPECT_DOUBLE_EQ(order.calculatePrice(), 10.99 + 8.50);
 }
+
+TEST(OrderTest, CalculateIngredientsTest)
+{
+    // Create instances of ConcreteMenuItem
+    std::unordered_map<std::string, int> ingredients1 = {{"flour", 2}, {"sugar", 1}};
+    std::unordered_map<std::string, int> ingredients2 = {{"flour", 2}, {"sugar", 1}};
+
+    std::shared_ptr<MenuItem> meal1 = std::make_shared<ConcreteMenuItem>(10.99, "Pancakes", ingredients1);
+    std::shared_ptr<MenuItem> meal2 = std::make_shared<ConcreteMenuItem>(8.50, "Waffles", ingredients2);
+
+    // Create an Order with the above meals
+    std::vector<std::shared_ptr<MenuItem>> meals = {meal1, meal2};
+    Order order(2, meals, std::make_shared<Waiter>());
+
+    // Calculate ingredients and check if they match the expected result
+    std::unordered_map<std::string, int> expectedIngredients = {{"flour", 4}, {"sugar", 2}};
+    EXPECT_EQ(order.calculateIngredients(), expectedIngredients);
+}
