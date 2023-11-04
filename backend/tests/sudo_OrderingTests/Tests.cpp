8b34651b4709ac651a775ce973e2a581391695fb
Gini24mp <u21573558@tuks.co.za>
2023-11-04 13:29:39 +0200

    order builder tests

--- backend/tests/sudo_OrderingTests/Tests.cpp
+++ backend/tests/sudo_OrderingTests/Tests.cpp
@@ -142,5 +142,46 @@ TEST(OrderTest, AddMealTest)
     EXPECT_EQ(order->getMeals()[2]->getName(), "Waffles");
 }
 
+TEST(OrderBuilderTest, CreateOrderTest) 
+{
+    std::shared_ptr<Management> management = std::make_shared<Management>();
+    std::shared_ptr<Kitchen> kitchen = std::make_shared<Kitchen>(management.get());
+    management->setKitchen(kitchen);
+    Section* section = management->getGeneralSection().get();
+    std::shared_ptr<Waiter> waiterPtr = std::make_shared<Waiter>(section, management.get());
+
+    // Create an instance of OrderBuilder
+    std::shared_ptr<OrderBuilder> orderBuilder = std::make_shared<OrderBuilder>();
+
+    // Set the table
+    std::shared_ptr<Table> table = section->getTables()[0];
+    orderBuilder->setTable(table);
+
+    // Set the waiter
+    orderBuilder->setWaiter(waiterPtr.get());
+
+    // Create instances of Food or Beverage as needed
+    std::unordered_map<std::string, int> ingredients1 = {{"flour", 2}, {"sugar", 1}};
+    std::shared_ptr<MenuItem> meal1 = std::make_shared<Food>(8.50, "Pancakes", ingredients1);
+
+    std::unordered_map<std::string, int> ingredients2 = {{"water", 1}, {"sugar", 2}};
+    std::shared_ptr<MenuItem> meal2 = std::make_shared<Beverage>(true, 2.99, "Cola", ingredients2);
+
+    // Add meals to the order
+    orderBuilder->addMeal(meal1);
+    orderBuilder->addMeal(meal2);
+
+    // Get the final order
+    std::shared_ptr<Order> order = orderBuilder->getOrder();
+
+    // Verify that the created order matches the expected values
+    EXPECT_EQ(order->getTable(), table);
+    EXPECT_EQ(order->getWaiter(), waiterPtr.get());
+    EXPECT_EQ(order->getMeals().size(), 2); // Assuming two meals were added
+
+    // Reset the OrderBuilder for creating a new order
+    orderBuilder->reset();
+
+}
 
 
