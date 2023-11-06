78a821c7f6a21f05644926604441a8c950fb362c
Gini24mp <u21573558@tuks.co.za>
2023-11-07 01:05:39 +0200

    git ingore update and addition of linter and automatic testing

--- backend/src/sudo_cooking/Cuisine.cpp
+++
@@ -1,8 +0,0 @@
-#include "Cuisine.h"
-
-Cuisine::Cuisine(double price,std::string name,std::string chef, std::unordered_map<std::string,int> ingredients) : Meal(price,name,chef,ingredients) {
-}
-
-Cuisine::Cuisine(const Cuisine& other) : Meal(other) { }
-
-Cuisine::~Cuisine() { }
--- backend/src/sudo_reservation/PrivateTable.cpp
+++
@@ -1,9 +0,0 @@
-#include "PrivateTable.h"
-
-PrivateTable::PrivateTable() : Table("Private", 6) {
-
-}
-
-PrivateTable::~PrivateTable() {
-
-}
