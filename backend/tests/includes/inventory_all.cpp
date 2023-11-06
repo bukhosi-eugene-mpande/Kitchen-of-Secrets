5b54a585a86dbace3e599fa644181dbc1e701838
Gini24mp <u21573558@tuks.co.za>
2023-11-07 00:37:22 +0200

    includes updated

--- backend/tests/includes/inventory_all.cpp
+++
@@ -1,2 +0,0 @@
-#include "../../src/sudo_accounting/Inventory.h"
-#include "../../src/sudo_accounting/Inventory.cpp"
--- backend/tests/includes/management_all.cpp
+++
@@ -1,2 +0,0 @@
-#include "../../src/sudo_management/Management.h"
-#include "../../src/sudo_management/Management.cpp"
